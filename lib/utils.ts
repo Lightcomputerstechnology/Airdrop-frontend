38195e926d8cf0f508f9a06a58200b6095ab1866
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-07-08 08:06:58 +0100

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,8 @@
 // lib/utils.ts
 
-export function cn(...inputs: (string | boolean | undefined | null)[]) {
-  return inputs.filter(Boolean).join(" ");
+import { clsx, ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(...inputs));
 }

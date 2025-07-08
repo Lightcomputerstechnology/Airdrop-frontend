e726b5d44069c1678ded1bcfde437b6792209146
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-07-08 07:32:21 +0100

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,27 @@
+// tailwind.config.ts
+
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
+
+// postcss.config.js
+
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};

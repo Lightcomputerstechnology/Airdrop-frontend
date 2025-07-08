90a61262bf072968e488a93ec5094163faafe42d
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-07-08 09:01:51 +0100

    Create toast-types.ts

---
+++ types/toast-types.ts
@@ -0,0 +1,7 @@
+// types/toast-types.ts
+
+import type { VariantProps } from "class-variance-authority";
+
+export interface ToastProps extends React.HTMLAttributes<HTMLDivElement>, VariantProps<any> {
+  variant?: "default" | "success" | "error";
+}
